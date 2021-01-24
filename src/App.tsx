57c7395d1729d5bfc5a156d808c6c399034c1f56
Mesko <willianmesko@hotmail.com>
2021-01-24 13:56:26 -0300

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { LastLocationProvider } from 'react-router-last-location';
 import AppProvider from '~/hooks';
-
+import LazyLoading from '~/components/LazyLoading';
 import Routes from '~/routes';
 
 import GlobalStyle from '~/styles/global';
 
 const App = () => (
-  <BrowserRouter>
+
+  <BrowserRouter >
     <LastLocationProvider>
       <AppProvider>
-        <Routes />
+        <Suspense fallback={<LazyLoading />}>
+          <Routes />
+        </Suspense>
       </AppProvider>
       <GlobalStyle />
     </LastLocationProvider>
   </BrowserRouter>
+
 );
 
 export default App;
---
+++ src/components/LazyLoading/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+
+
+const LazyLoading: React.FC = () => {
+    return (
+        <Container>
+            <h1>LOADING..</h1>
+        </Container>
+    );
+};
+
+export default LazyLoading;
